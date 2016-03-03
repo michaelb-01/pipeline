8d7f5b615eaac0be34b7abb4f680a8ac395a3310
Michael Battcock <michaelbattcock@Michaels-MacBook-Pro.local>
2016-03-03 08:24:44 -0800

    Added all page templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,12 +2,13 @@
 module.exports.homelist = function(req, res){
   res.render('locations-list', { title: 'Home' });
 };
+
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res){
-  res.render('index', { title: 'Location info' });
+  res.render('location-info', { title: 'Location info' });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-  res.render('index', { title: 'Add review' });
+  res.render('location-review-form', { title: 'Add review' });
 };
